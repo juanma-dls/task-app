fe7ce8f0876a894652658bea30e3c56f3c4cfd8f
Juanma De los Santos <juanmadls6@gmail.com>
2024-02-24 01:47:42 -0300

    changes in seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,12 @@ roles = Rol.create!([
 ])
 
 Subrol.create!([
-  { name: "Desarrollador Front-end", description: "Responsable del desarrollo de la interfaz de usuario." },
-  { name: "Desarrollador Back-end", description: "Responsable del desarrollo del servidor y la lógica de la aplicación." },
-  { name: "Diseñador UX/UI", description: "Responsable del diseño visual y la experiencia de usuario de la aplicación." },
-  { name: "QA Manual", description: "Responsable de las pruebas manuales de la aplicación." },
-  { name: "QA Automatización", description: "Responsable de la automatización de las pruebas de la aplicación." },
+  { name: "Desarrollo Front-end", description: "Responsable del desarrollo de la interfaz de usuario." },
+  { name: "Desarrollo Back-end", description: "Responsable del desarrollo del servidor y la lógica de la aplicación." },
+  { name: "Diseño UX/UI", description: "Responsable del diseño visual y la experiencia de usuario de la aplicación." },
+  { name: "Unit Test", description: "Generación de Test para los métodos" },
   { name: "Gestor de proyecto", description: "Responsable de la planificación, organización y seguimiento del proyecto." },
-  { name: "Scrum Master", description: "Responsable de la implementación de la metodología Scrum en el proyecto." },
-  { name: "Product Owner", description: "Responsable de definir la visión y las prioridades del producto." }
+  { name: "Observador", description: "Podra realizar seguimiento de la tarea sin intervenir" },
 ])
 
 User.create!([
